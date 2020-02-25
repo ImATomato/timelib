3690059b475b21b26c0751a43aa3bfe2f3d4dfba
ImATomato <60101397+ImATomato@users.noreply.github.com>
2020-02-25 10:43:03 +0100

    Added inputdate,comments

--- timelib.h
+++ timelib.h
@@ -1,3 +1,8 @@
+/**
+* Autor: Alexander Kötter
+* Datum: 2020-02-11
+* Last edit: 2020-02-25
+**/
 #ifndef TIMELIB_H_INCLUDED
 #define TIMELIB_H_INCLUDED
 int day_of_the_year(int day,int month, int year);
@@ -5,6 +10,6 @@ int exists_date(int day,int month, int year);
 int get_days_for_month(int month, int year);
 int is_leapyear(int year);
 int weekday(year);
-
+void input_date(int *day, int *month, int *year);
 
 #endif // TIMELIB_H_INCLUDED
